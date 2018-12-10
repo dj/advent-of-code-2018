3a9975bafd566fbeeb4b87c2d1060ac54284fc82
dj <danieljayhartman@gmail.com>
2018-12-09 23:09:36 -0500

    Day 2 part 2

--- day_2/day_2.go
+++ day_2/day_2.go
@@ -1,19 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
-	"bufio"
 	"strings"
 )
 
-func main() {
-	content, err :=	 ioutil.ReadFile("day_2.txt")
-	if err != nil {
-		panic(err)
-	}
-	input := string(content[:])
-
+func part1(input string) int {
 	scanner := bufio.NewScanner(strings.NewReader(input))
 	has2 := 0
 	has3 := 0
@@ -46,5 +40,54 @@ func main() {
 			runesChecked[rune] = true
 		}
 	}
-	fmt.Println("checksum: ", has2 * has3)
+	return has2 * has3
+}
+
+func part2(input string) string {
+	scanner := bufio.NewScanner(strings.NewReader(input))
+	lineCount := 0
+	lines := make(map[int]string)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines[lineCount] = line
+		lineCount = lineCount + 1
+	}
+
+	for _, line1 := range lines {
+		for _, line2 := range lines {
+			mismatches := 0
+
+			for i := 0; i < len(line1); i++ {
+				if line1[i] != line2[i] {
+					mismatches++
+				}
+				if mismatches >= 2 {
+					break
+				}
+			}
+			if mismatches == 1 {
+				common := ""
+				for i := 0; i < len(line1); i++ {
+					if line1[i] == line2[i] {
+						common = common + string(line1[i])
+					}
+				}
+				return common
+			}
+		}
+	}
+
+	return ""
+}
+
+func main() {
+	content, err := ioutil.ReadFile("day_2.txt")
+	if err != nil {
+		panic(err)
+	}
+	input := string(content[:])
+
+	fmt.Println("part 1: ", part1(input))
+	fmt.Println("part 2: ", part2(input))
 }
